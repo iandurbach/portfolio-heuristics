786e62d2958417ed10874a4bbe68e2ca83b420ed
simonalgorta <simalgorta@gmail.com>
2017-07-05 16:51:34 +0200

    fixed bug introduced in last commit on makedata.r

--- results/makedata.R
+++ results/makedata.R
@@ -5,10 +5,7 @@ library(plyr)
 
 
 renameModels <- function(x)
-  revalue(x, c("opt_nor"="Optimal", "min_nor" = "Nadir", "ttb_nor" = "Greedy", "greedy_net_nor"="Greedy Net", "dom_nor" = "Heuristic", "rand_nor" = "Random", "greedynet_nor" = "greedynet", "greedyvalue_nor" = "greedyvalue", "greedycost_nor" = "greedycost", "opt"="Optimal", "min" = "Nadir", "ttb" = "Greedy", "dom" = "Heuristic","greedy_netvalue"="Greedy Net", "rand" = "Random", "mvpmax" = "mvp", "lvpmax" = "lvp", "mvpmax_nor" = "mvp", "lvpmax_nor" = "lvp", "rvpmax" = "rvp", "rvpmax_nor" = "rvp"))
-
-se <- function(x)
-  se=sd(x)/sqrt(length(x))
+  revalue(x, c("opt_nor"="Optimal", "min_nor" = "Nadir", "ttb_nor" = "Greedy", "dom_nor" = "Heuristic", "rand_nor" = "Random", "greedynet_nor" = "greedynet", "greedyvalue_nor" = "greedyvalue", "greedycost_nor" = "greedycost", "opt"="Optimal", "min" = "Nadir", "ttb" = "Greedy", "dom" = "Heuristic", "rand" = "Random", "mvpmax" = "mvp", "lvpmax" = "lvp", "mvpmax_nor" = "mvp", "lvpmax_nor" = "lvp", "rvpmax" = "rvp", "rvpmax_nor" = "rvp"))
 
 Xall = c()
 Xall = rbind(Xall, cbind(read.csv("results/base_context_3a_neg.csv"), data = "negative"))
@@ -24,14 +21,14 @@ Xall = rbind(Xall, cbind(read.csv("results/base_context_3c_uni.csv"), data = "un
 normalized <- Xall[c(1,2,3,4,5,6,7,8,9,21,22,23,24,25,26,27,28,29,30,31,32)]
 normalized <- melt(normalized, id = c("X", "nproj", "nCV", "budget", "my_alpha", "my_gamma","my_selprob", "random_nested", "interaction_pool", "data"))
 
-absolute <- Xall[c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17, 18,19,20,21,32)]#Absolute values
+absolute <- Xall[c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17, 18,19,20,32)]#Absolute values
 absolute <- melt(absolute, id = c("X", "nproj", "nCV", "budget", "my_alpha", "my_gamma","my_selprob","random_nested", "interaction_pool", "data"))
 
 dat <- rbind(mutate(normalized, normalized = "Normalized"), mutate(absolute, normalized = "Absolute"))
 dat$budget <- round(dat$budget, 2)
 dat$variable<- renameModels(dat$variable)
 dat$my_alpha = revalue(factor(dat$my_alpha), c("0"="No Interactions", "3"="With Interactions"))
-dat$my_gamma = revalue(factor(dat$my_gamma), c("0"="No Multiplicative Interactions", "1"="Low Multiplicative Interactions", "2"="High Multiplicative Interactions"))
+dat$my_gamma = revalue(factor(dat$my_gamma), c("0"="No Multiplicative Interactions", "0.5"="Low Multiplicative Interactions", "1"="High Multiplicative Interactions"))
 dat$random_nested = revalue(factor(dat$random_nested), c("0"="Random", "1"="Nested"))
 dat$my_selprob = revalue(factor(dat$my_selprob), c("1"="Neutral", "2"="Goodish", "3"="Poorish"))
 
